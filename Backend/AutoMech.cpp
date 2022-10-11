014b9ff5617b1cef4aed392c8d533a2a35d10115
adamqvst <90694238+adamqvst@users.noreply.github.com>
2022-10-11 13:44:27 +0300

    Mini update

--- Backend/AutoMech.cpp
+++ Backend/AutoMech.cpp
@@ -1,8 +1,17 @@
-// Your First C++ Program
+// Your First C++ Program xd
+
+/*
+g++ -S AutoMech.cpp (bara ifall du vill se assembly, very cool)
+
+Kompilera:
+g++ AutoMech.cpp -o AutoMech
+Kör:
+./AutoMech
+*/
 
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "AutoMech v0";
     return 0;
 }
