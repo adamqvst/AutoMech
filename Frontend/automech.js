162f03273673498a03a90deb5644266b386d8c67
jr1234444 <jacob.rosing@abo.fi>
2022-11-06 15:30:27 +0200

    added working runtime counter along with start and stop buttons

--- Frontend/automech.js
+++ Frontend/automech.js
@@ -7,8 +7,41 @@ cyl4 = 0;
 
 cyl = "placeholder"
 
-function runtimeUpdate(){
-    document.getElementById('runtime').innerHTML = "3600(placeholder)";
+var start;
+var runtime;
+var seconds;
+var miliseconds;
+
+//function below is needed since calling setInterval overwrites any previous interval ID making it inaccessible to the clearInterval function
+function disableStartBtn(){
+    document.getElementById("start").disabled = true;
+}
+
+function runtimeCount(){
+        start = new Date();
+        runtimeUpdate = setInterval(runtime, 1000);
+}
+
+function runtime(){
+    miliseconds = new Date() - start;
+    seconds = Math.trunc(miliseconds / 1000);
+    document.getElementById('runtime').innerHTML = seconds;
+}
+
+function stop(){
+    clearInterval(runtimeUpdate);
+    runtimeUpdate=null;
+    while (runtimeUpdate!== null){
+        runtimeUpdate = null;
+    }
+    document.getElementById('runtime').innerHTML = 0;
+    document.getElementById('rpm').innerHTML = 0;
+    document.getElementById('totmisfires').innerHTML = 0;
+    document.getElementById('misfires1').innerHTML = 0;
+    document.getElementById('misfires2').innerHTML = 0;
+    document.getElementById('misfires3').innerHTML = 0;
+    document.getElementById('misfires4').innerHTML = 0;
+    document.getElementById("start").disabled = false;
 }
 
 function rpmUpdate(){
