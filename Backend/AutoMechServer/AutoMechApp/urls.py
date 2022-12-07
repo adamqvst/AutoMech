0e53e30f58ee18493cfb3c8d313c36a68471c4c3
osk4r99 <osk4r99@gmail.com>
2022-12-07 15:02:01 +0200

    added url for inpit device

--- Backend/AutoMechServer/AutoMechApp/urls.py
+++ Backend/AutoMechServer/AutoMechApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('api/begin-diagnostics', views.begin_diagnostics, name='begin_diagnostics'),
     path('api/end-diagnostics', views.end_diagnostics, name='end_diagnostics'),
-    path('api/get-diagnostics-data', views.get_diagnostics_data, name='get_diagnostics_data')
+    path('api/get-diagnostics-data', views.get_diagnostics_data, name='get_diagnostics_data'),
+    path('api/get_input_devices', views.get_input_devices, name='get_input_devices')
 ]
