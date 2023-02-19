8784357735e6b568265ee0b87bbff43a8c6e5d63
adamqvst <adam.lindqvist@abo.fi>
2023-02-19 16:41:31 +0200

    created an independent js file (graph_toolkit.js) to make graph.js more readable

--- Frontend/js/graph/graph.js
+++ Frontend/js/graph/graph.js
@@ -9,9 +9,10 @@ var gl;
 var canvas;
 var grid_shader_program;
 var graph_shader_program;
+var crosshair_shader_program;
 var graphs = new Map();
 
-/* update viewports */
+/* Update viewports */
 function updateViewports() {
 
     canvas.width = window.innerWidth;
@@ -59,123 +60,6 @@ function createGraph(id, data, dataPointFunction, gridSize) {
     return g;
 }   
 
-/* Generates data for rendering a grid pattern */
-function constructGrid(gridSize, width, height) {
-    let cols = Math.ceil(width / gridSize);
-    let rows = Math.ceil(height / gridSize);
-
-    if (rows % 2 == 0) {
-        rows--;
-    }
-
-    let s = (gridSize / height) * 2;
-    let n_points = (rows * 2 + cols * 2) * 2;
-    let grid_data = new Float32Array(n_points);
-    let aspectRatio = width / height;
-
-    // columns
-    for (let i = 0; i < cols; i++) {
-        //TOP
-        grid_data[i * 4] = aspectRatio - i * s - s;
-        grid_data[i * 4 + 1] = 1.0;
-
-        //BOTTOM
-        grid_data[i * 4 + 2] = aspectRatio - i * s - s;
-        grid_data[i * 4 + 3] = -1.0;
-    }
-
-    // rows
-    for (let i = 0; i < rows; i++) {
-        //LEFT
-        grid_data[cols * 4 + i * 4] = -aspectRatio;
-        grid_data[cols * 4 + i * 4 + 1] = -i * s + (s * rows) / 2.0 - 0.5 * s;
-
-        //RIGHT
-        grid_data[cols * 4 + i * 4 + 2] = aspectRatio;
-        grid_data[cols * 4 + i * 4 + 3] = -i * s + (s * rows) / 2.0 - 0.5 * s;
-    }
-
-    return grid_data;
-}
-
-/* Converts data into a format which can be rendered as a graph */
-// Use this function when plotting data with a chunk size of 1, e.g. engine rpm (rpm_data)
-function continuous_DPF(data, n_chunks, chunk_size, aspectRatio) {
-
-    let dataPoints = new Float32Array(2 * n_chunks);
-
-    if (data.length > 0) {
-        for (let i = 0; i < n_chunks; i++) {
-            let spacing = aspectRatio * 2.0 / (n_chunks - 1.0);
-            dataPoints[2 * i] = i * spacing - aspectRatio; // X
-            dataPoints[2 * i + 1] = data[i] / 10000; // Y,  (divided by max rpm)
-        }
-    }
-
-    return dataPoints;
-}
-
-/* Converts data into a format which can be rendered as a graph */
-// Use this function when plotting data with a chunk size greater than 1, e.g. engine audio (wave_data)
-function chunked_DPF(data, n_chunks, chunk_size, aspectRatio) {
-
-    /* Current number of datapoints. Ranges from 0 to n_chunks * chunk_size depending on the current number of loaded chunks */
-    let dataPoints = new Float32Array(chunk_size * 2 * n_chunks);   // doubled because we need to store both the x- and y-coordinates
-
-    if (data.length > 0) {
-        //for each chunk
-        for (let i = 0; i < n_chunks; i++) {
-            let chunk_spacing = aspectRatio * (i / n_chunks) * 2;
-            //for each number in chunk
-            for (let j = chunk_size - 1; j >= 0; j--) {
-                let spacing = aspectRatio * 2.0 / (chunk_size * n_chunks - 1.0);
-                dataPoints[2 * i * chunk_size + j * 2] = j * spacing + chunk_spacing - aspectRatio;   //x-coordinate
-                dataPoints[2 * i * chunk_size + j * 2 + 1] = data[data.length - n_chunks + i][j] / 32767; //y-coordinate, max-size of 16bit int
-                dataPoints[2 * i * chunk_size + j * 2 + 1] *= 0.1;   //Amplitude
-            }
-        }
-    }
-    return dataPoints;
-}
-
-async function fetchShaderSource(shader_type) {
-    return fetch('http://127.0.0.1:8000/static/js/graph/' + shader_type, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "text/plain"
-        },
-    }).then(res => {
-        return res.text();
-    });
-}
-
-function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-        return shader;
-    }
-
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
-
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
-
 /* Fetches shader source code and creates shader programs, must be run before any graphs can be created*/
 async function initialize_graphs() {
 
@@ -195,6 +79,8 @@ async function initialize_graphs() {
     var graph_FS_source = await fetchShaderSource('graph_FS.frag');
     var grid_VS_source = await fetchShaderSource('grid_VS.vert');
     var grid_FS_source = await fetchShaderSource('grid_FS.frag');
+    var crosshair_VS_source = await fetchShaderSource('crosshair_VS.vert');
+    var crosshair_FS_source = await fetchShaderSource('crosshair_FS.frag');
 
     // Create shader program
     let vertexShader = createShader(gl, gl.VERTEX_SHADER, grid_VS_source);
@@ -204,6 +90,10 @@ async function initialize_graphs() {
     vertexShader = createShader(gl, gl.VERTEX_SHADER, graph_VS_source);
     fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, graph_FS_source);
     graph_shader_program = createProgram(gl, vertexShader, fragmentShader);
+
+    vertexShader = createShader(gl, gl.VERTEX_SHADER, crosshair_VS_source);
+    fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, crosshair_FS_source);
+    crosshair_shader_program = createProgram(gl, vertexShader, fragmentShader);
 
     main_loop();
 }
@@ -233,24 +123,30 @@ function renderGraphs() {
 // gridSize: the width in pixels of the grid squares
 class Graph {
 
-    dataPoints;
-    max_n_storedChunks = 20;
-    graphColor = { r: 1.0, g: 1.0, b: 1.0, a: 1.0}
-    paperColor = { r: 0.267, g: 0.267, b: 0.267, a: 1.0 };
-    gridColor = { r: 0.0, g: 0.0, b: 0.0, a: 1.0 };
-    graphScale = { h: 1.0, v: 1.0 };
+    _crosshairData = new Float32Array([0.0, 0.5, 0.0, -0.5,   // vertical line
+                                     -0.5, 0.0, 0.5, 0.0]);   // horizontal line
+    _dataPoints;
+    _max_n_storedChunks = 20;
+    _crosshairColor = { r: 1.0, g: 0.0, b: 0.0, a: 1.0 };
+    _graphColor = { r: 1.0, g: 1.0, b: 1.0, a: 1.0 }
+    _paperColor = { r: 0.267, g: 0.267, b: 0.267, a: 1.0 };
+    _gridColor = { r: 0.0, g: 0.0, b: 0.0, a: 1.0 };
+    _graphScale = { h: 1.0, v: 1.0 };
 
     constructor(id, dataSource, dataPointFunction, gridSize, graphPaper) {
         this.id = id;   
         this.dataSource = dataSource;
         this.dataPointFunction = dataPointFunction;
+        this.crosshair_VAO = gl.createVertexArray();
         this.graph_VAO = gl.createVertexArray();
         this.grid_VAO = gl.createVertexArray();
+        this.crosshair_VBO = gl.createBuffer();
         this.graph_VBO = gl.createBuffer();
         this.grid_VBO = gl.createBuffer();
         this.gridSize = gridSize;
         this.graphPaper = graphPaper;
         this.gridData = constructGrid(gridSize, graphPaper.clientWidth, graphPaper.clientHeight);
+
         gl.bindVertexArray(this.grid_VAO);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.grid_VBO);
         gl.vertexAttribPointer(gl.getAttribLocation(grid_shader_program, "a_position"), 2, gl.FLOAT, false, 0, 0);  // TODO: store attribute location out of function scope
@@ -259,23 +155,36 @@ class Graph {
     }
 
     setMaxChunks(n) {
-        this.max_n_storedChunks = n;
+        this._max_n_storedChunks = n;
+    }
+
+    setCrosshairColor(r, g, b, a) {
+        this._graphColor = { r: r, g: g, b: b, a: a };
     }
 
     setGraphColor(r, g, b, a) {
-        this.graphColor = { r: r, g: g, b: b, a: a };
+        this._graphColor = { r: r, g: g, b: b, a: a };
     }
 
     setPaperColor(r, g, b, a) {
-        this.paperColor = { r: r, g: g, b: b, a: a };
+        this._paperColor = { r: r, g: g, b: b, a: a };
     }
 
     setGridColor(r, g, b, a) {
-        this.gridColor = { r: r, g: g, b: b, a: a };
+        this._gridColor = { r: r, g: g, b: b, a: a };
     }
 
     setGraphScale(h, v) {
-        this.graphScale = { h: h, v: v };
+        this._graphScale = { h: h, v: v };
+    }
+
+    setDataSource(dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    getAspectRatio() {
+        let rect = this.graphPaper.getBoundingClientRect();
+        return rect.width / rect.height;
     }
 
     updateViewport() {
@@ -297,12 +206,14 @@ class Graph {
         const left = rect.left;
         const bottom = canvas.clientHeight - rect.bottom;
      
+        let view = Matrix4x4.createViewMatrix(1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, this.getAspectRatio());
+
         gl.viewport(left, bottom, rect.width, rect.height);     
 
         // Clear previous frame
         gl.enable(gl.SCISSOR_TEST);
         gl.scissor(left, bottom, rect.width, rect.height);
-        gl.clearColor(this.paperColor.r, this.paperColor.g, this.paperColor.b, this.paperColor.a);
+        gl.clearColor(this._paperColor.r, this._paperColor.g, this._paperColor.b, this._paperColor.a);
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.disable(gl.SCISSOR_TEST);
 
@@ -310,18 +221,19 @@ class Graph {
         gl.bindVertexArray(this.grid_VAO);
         gl.useProgram(grid_shader_program);
         gl.uniform2i(gl.getUniformLocation(grid_shader_program, "u_windowSize"), this.graphPaper.clientWidth, this.graphPaper.clientHeight);
-        gl.uniform4f(gl.getUniformLocation(grid_shader_program, "u_gridColor"), this.gridColor.r, this.gridColor.g, this.gridColor.b, this.gridColor.a);
+        gl.uniformMatrix4fv(gl.getUniformLocation(grid_shader_program, "u_viewMatrix"), true, view);
+        gl.uniform4f(gl.getUniformLocation(grid_shader_program, "u_gridColor"), this._gridColor.r, this._gridColor.g, this._gridColor.b, this._gridColor.a);
         gl.drawArrays(gl.LINES, 0, this.gridData.length / 2);
 
         // Render graph
         gl.bindVertexArray(this.graph_VAO);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.graph_VBO);
-        gl.vertexAttribPointer(gl.getAttribLocation(graph_shader_program, "a_position"), 2, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(gl.getAttribLocation(graph_shader_program, "a_position"), 2, gl.FLOAT, true, 0, 0);
         gl.enableVertexAttribArray(gl.getAttribLocation(graph_shader_program, "a_position"));
 
         let n_chunks = this.dataSource.length;
-        if (this.dataSource.length > this.max_n_storedChunks) {
-            n_chunks = this.max_n_storedChunks;
+        if (this.dataSource.length > this._max_n_storedChunks) {
+            n_chunks = this._max_n_storedChunks;
         }
 
         var chunkSize = 0;
@@ -332,19 +244,34 @@ class Graph {
             chunkSize = 1;
         }
 
-        this.dataPoints = this.dataPointFunction(this.dataSource, n_chunks, chunkSize, rect.width / rect.height);
+        this._dataPoints = this.dataPointFunction(this.dataSource, n_chunks, chunkSize, rect.width / rect.height);
 
-        if (this.dataPoints != undefined) {
+        if (this._dataPoints != undefined) {
             gl.bindBuffer(gl.ARRAY_BUFFER, this.graph_VBO);
-            gl.bufferData(gl.ARRAY_BUFFER, this.dataPoints, gl.STATIC_DRAW);
+            gl.bufferData(gl.ARRAY_BUFFER, this._dataPoints, gl.STATIC_DRAW);
 
             gl.bindVertexArray(this.graph_VAO);
             gl.useProgram(graph_shader_program);
-            gl.uniform4f(gl.getUniformLocation(graph_shader_program, "u_graphColor"), this.graphColor.r, this.graphColor.g, this.graphColor.b, this.graphColor.a);
+            gl.uniformMatrix4fv(gl.getUniformLocation(graph_shader_program, "u_viewMatrix"), true, view);
+            gl.uniform4f(gl.getUniformLocation(graph_shader_program, "u_graphColor"), this._graphColor.r, this._graphColor.g, this._graphColor.b, this._graphColor.a);
             gl.uniform2i(gl.getUniformLocation(graph_shader_program, "u_windowSize"), rect.width, rect.height);
-            gl.uniform2f(gl.getUniformLocation(graph_shader_program, "u_graphScale"), this.graphScale.h, this.graphScale.v);
+            gl.uniform2f(gl.getUniformLocation(graph_shader_program, "u_graphScale"), this._graphScale.h, this._graphScale.v);
             const n_vertices = n_chunks * chunkSize;
             gl.drawArrays(gl.LINE_STRIP, 0, n_vertices);
         }
+
+        // Render crosshair
+        let transformation = Matrix4x4.createTransformationMatrix(1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
+        gl.bindVertexArray(this.crosshair_VAO);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.crosshair_VBO);
+        gl.vertexAttribPointer(gl.getAttribLocation(crosshair_shader_program, "a_position"), 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(gl.getAttribLocation(crosshair_shader_program, "a_position"));
+        gl.bufferData(gl.ARRAY_BUFFER, this._crosshairData, gl.STATIC_DRAW);
+        gl.useProgram(crosshair_shader_program);
+        gl.uniformMatrix4fv(gl.getUniformLocation(crosshair_shader_program, "u_viewMatrix"), true, view);
+        gl.uniformMatrix4fv(gl.getUniformLocation(crosshair_shader_program, "u_transformationMatrix"), true, transformation);
+        gl.uniform2i(gl.getUniformLocation(crosshair_shader_program, "u_windowSize"), rect.width, rect.height);
+        gl.uniform4f(gl.getUniformLocation(crosshair_shader_program, "u_crosshairColor"), this._crosshairColor.r, this._crosshairColor.g, this._crosshairColor.b, this._crosshairColor.a);
+        gl.drawArrays(gl.LINES, 0, this._crosshairData.length / 2);
     }   
 }
---
+++ Frontend/js/graph/graph_toolkit.js
@@ -0,0 +1,177 @@
+////---------------------------------------------////
+//            G R A P H - T O O L K I T            //
+////---------------------------------------------////
+
+"use strict";
+
+/* Generates data for rendering a grid pattern */
+function constructGrid(gridSize, width, height) {
+    let cols = Math.ceil(width / gridSize);
+    let rows = Math.ceil(height / gridSize);
+
+    if (rows % 2 == 0) {
+        rows--;
+    }
+
+    let s = (gridSize / height) * 2;
+    let n_points = (rows * 2 + cols * 2) * 2;
+    let grid_data = new Float32Array(n_points);
+    let aspectRatio = width / height;
+
+    // columns
+    for (let i = 0; i < cols; i++) {
+        //TOP
+        grid_data[i * 4] = aspectRatio - i * s - s;
+        grid_data[i * 4 + 1] = 1.0;
+
+        //BOTTOM
+        grid_data[i * 4 + 2] = aspectRatio - i * s - s;
+        grid_data[i * 4 + 3] = -1.0;
+    }
+
+    // rows
+    for (let i = 0; i < rows; i++) {
+        //LEFT
+        grid_data[cols * 4 + i * 4] = -aspectRatio;
+        grid_data[cols * 4 + i * 4 + 1] = -i * s + (s * rows) / 2.0 - 0.5 * s;
+
+        //RIGHT
+        grid_data[cols * 4 + i * 4 + 2] = aspectRatio;
+        grid_data[cols * 4 + i * 4 + 3] = -i * s + (s * rows) / 2.0 - 0.5 * s;
+    }
+
+    return grid_data;
+}
+
+/* Converts data into a format which can be rendered as a graph */
+// Use this function when plotting data with a chunk size of 1, e.g. engine rpm (rpm_data)
+function continuous_DPF(data, n_chunks, chunk_size, aspectRatio) {
+
+    let dataPoints = new Float32Array(2 * n_chunks);
+
+    if (data.length > 0) {
+        for (let i = 0; i < n_chunks; i++) {
+            let spacing = aspectRatio * 2.0 / (n_chunks - 1.0);
+            dataPoints[2 * i] = i * spacing - aspectRatio; // X
+            dataPoints[2 * i + 1] = data[i] / 10000; // Y,  (divided by max rpm)
+        }
+    }
+
+    return dataPoints;
+}
+
+/* Converts data into a format which can be rendered as a graph */
+// Use this function when plotting data with a chunk size greater than 1, e.g. engine audio (wave_data)
+function chunked_DPF(data, n_chunks, chunk_size, aspectRatio) {
+
+    /* Current number of datapoints. Ranges from 0 to n_chunks * chunk_size depending on the current number of loaded chunks */
+    let dataPoints = new Float32Array(chunk_size * 2 * n_chunks);   // doubled because we need to store both the x- and y-coordinates
+
+    if (data.length > 0) {
+        //for each chunk
+        for (let i = 0; i < n_chunks; i++) {
+            let chunk_spacing = aspectRatio * (i / n_chunks) * 2;
+            //for each number in chunk
+            for (let j = chunk_size - 1; j >= 0; j--) {
+                let spacing = aspectRatio * 2.0 / (chunk_size * n_chunks - 1.0);
+                dataPoints[2 * i * chunk_size + j * 2] = j * spacing + chunk_spacing - aspectRatio;   //x-coordinate
+                dataPoints[2 * i * chunk_size + j * 2 + 1] = data[data.length - n_chunks + i][j] / 32767; //y-coordinate, max-size of 16bit int
+                dataPoints[2 * i * chunk_size + j * 2 + 1] *= 0.1;   //Amplitude
+            }
+        }
+    }
+    return dataPoints;
+}
+
+async function fetchShaderSource(shader_type) {
+    return fetch('http://127.0.0.1:8000/static/js/graph/' + shader_type, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "text/plain"
+        },
+    }).then(res => {
+        return res.text();
+    });
+}
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+class Matrix4x4 {
+
+    constructor() {
+        this.data = new Float32Array(16);
+    }
+
+    translate(dx, dy, dz) {
+        this.data[3] += dx;
+        this.data[7] += dy;
+        this.data[11] += dz;
+    }
+
+    setTranslation(tx, ty, tz) {
+        this.data[3] = tx;
+        this.data[7] = ty;
+        this.data[11] = tz;
+    }
+
+    setScale(sx, sy, sz) {
+        this.data[0] = sx;
+        this.data[5] = sy;
+        this.data[10] = sz;
+    }
+
+    static identity() {
+        return new Float32Array([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]);
+    }
+
+    // Temporary
+    static createViewMatrix(sx, sy, sz, tx, ty, tz, zoom, aspectRatio) {
+        return new Float32Array([
+            sx * zoom / aspectRatio, 0, 0, tx,
+            0, sy * zoom, 0, ty,
+            0, 0, sz, tz,
+            0, 0, 0, 1.0
+        ]);
+    }
+
+    // Temporary
+    static createTransformationMatrix(sx, sy, sz, tx, ty, tz) {
+        return new Float32Array([
+            sx, 0, 0, tx,
+            0, sy, 0, ty,
+            0, 0, sz, tz,
+            0, 0, 0, 1.0
+        ]);
+    }
+}
+
